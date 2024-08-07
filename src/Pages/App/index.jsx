322a8422fc635da2aa0ecb746bf933b375dce27d
Sebastián Riveros <seba.arp@outlook.com>
2024-08-06 23:38:45 -0400

    Hooks

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,3 +1,4 @@
+import { useRoutes, BrowserRouter } from "react-router-dom";
 import Home from "../Home";
 import MyAccount from "../MyAccount";
 import MyOrder from "../MyOrder";
@@ -6,16 +7,23 @@ import NotFound from "../NotFound";
 import SignIn from "../SignIn";
 import "./App.css";
 
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "/my-account", element: <MyAccount /> },
+    { path: "/my-order", element: <MyOrder /> },
+    { path: "/my-orders", element: <MyOrders /> },
+    { path: "/*", element: <NotFound /> },
+    { path: "/signin", element: <SignIn /> },
+  ]);
+  return routes;
+};
+
 function App() {
   return (
-    <div className="bg-red-100">
-      <Home />
-      <MyAccount />
-      <MyOrder />
-      <MyOrders />
-      <NotFound />
-      <SignIn />
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
