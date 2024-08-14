3a876c8d20aecf67e1e82504fa73d82181ee1ed6
Sebastián Riveros <seba.arp@outlook.com>
2024-08-13 23:59:32 -0400

    Context

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,10 +1,20 @@
+import { XMarkIcon } from "@heroicons/react/24/solid";
 import "./styles.css";
+import { useContext } from "react";
+import { ShoppingCartContext } from "../../Context";
 const ProductDetail = () => {
+  const context = useContext(ShoppingCartContext);
   return (
-    <aside className="product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white">
+    <aside
+      className={`${
+        context.isProductDetailOpen ? "flex" : "hidden"
+      } product-detail flex-col fixed right-0 border border-black rounded-lg bg-white`}
+    >
       <div className="flex justify-between items-center p-6">
         <h2 className="font-medium text-xl">Detail</h2>
-        <div>X</div>
+        <div>
+          <XMarkIcon className="h-6 w-6 text-blue-500"></XMarkIcon>
+        </div>
       </div>
     </aside>
   );
