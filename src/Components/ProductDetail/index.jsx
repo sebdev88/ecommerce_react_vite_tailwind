17387fa4b4d4b51df83146856c59266eff9378e8
Sebastián Riveros <seba.arp@outlook.com>
2024-08-18 01:07:56 -0400

    Product detail recibiendo datos de la api, también comportamiento de clic para cerrar, también tiene el cursor distinto cuando pasa por la X

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -2,8 +2,11 @@ import { XMarkIcon } from "@heroicons/react/24/solid";
 import "./styles.css";
 import { useContext } from "react";
 import { ShoppingCartContext } from "../../Context";
+
+//Detalle de producto
 const ProductDetail = () => {
   const context = useContext(ShoppingCartContext);
+
   return (
     <aside
       className={`${
@@ -13,9 +16,30 @@ const ProductDetail = () => {
       <div className="flex justify-between items-center p-6">
         <h2 className="font-medium text-xl">Detail</h2>
         <div>
-          <XMarkIcon className="h-6 w-6 text-blue-500"></XMarkIcon>
+          <XMarkIcon
+            className="h-6 w-6 text-blue-500 cursor-pointer"
+            onClick={() => context.closeProductDetail()}
+          ></XMarkIcon>
         </div>
       </div>
+      <figure className="px-6">
+        <img
+          className="w-full h-full rounded-lg"
+          src={context.productToShow.images[0]}
+          alt={context.productToShow.title}
+        />
+      </figure>
+      <p className="flex flex-col p-6">
+        <span className="font-medium text-2xl mb-2">
+          ${context.productToShow.price}
+        </span>
+        <span className="font-medium text-md">
+          {context.productToShow.title}
+        </span>
+        <span className="font-light text-sm">
+          {context.productToShow.description}
+        </span>
+      </p>
     </aside>
   );
 };
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -3,19 +3,27 @@ import { createContext, useState } from "react";
 export const ShoppingCartContext = createContext();
 
 export const ShoppingCartProvider = ({ children }) => {
+  //incrementa cantidad del ShoppingCart
   const [count, setCount] = useState(0);
-  const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
 
+  //Detalle de producto, abrir cerrar
+  const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
   const openProductDetail = () => setIsProductDetailOpen(true);
   const closeProductDetail = () => setIsProductDetailOpen(false);
+
+  //Product Detail Mostrar Productos
+  const [productToShow, setProductToShow] = useState({}); //Objeto vacío
+
   return (
-    <ShoppingCartContext.Provider
+    <ShoppingCartContext.Provider //Provider de contexto que está más arriba.
       value={{
         count,
         setCount,
         openProductDetail,
         closeProductDetail,
         isProductDetailOpen,
+        productToShow,
+        setProductToShow,
       }}
     >
       {children}
